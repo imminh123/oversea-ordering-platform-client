08a9019fc7ec701249d6cb8143312dd69cbf332f
alethienvu <ale.thienvu@setel.com>
2023-11-09 22:51:19 +0700

    add taildwind

---
+++ dist/assets/index-83abe111.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-450372a5.js";const t=()=>r.jsx("div",{children:"Feature 2"});export{t as Feature2};
---
+++ dist/assets/index-b5f004be.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-450372a5.js";const t=()=>r.jsx("div",{children:"feature 3"});export{t as Feature3};
